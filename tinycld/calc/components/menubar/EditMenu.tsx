fc0b452e92ca8db8657ad5672b99716905fcf091
Nathan Stitt <nathan@stitt.org>
2026-05-13 17:06:48 -0500

    feat(calc): Edit menu with undo/redo, cut/copy/paste, find

--- tinycld/calc/components/menubar/EditMenu.tsx
+++ tinycld/calc/components/menubar/EditMenu.tsx
@@ -1,14 +1,55 @@
-import { Menu } from '@tinycld/core/ui/menu'
+import { Menu, Separator } from '@tinycld/core/ui/menu'
 import type { MenuBarProps } from './MenuBar'
 import { MenuBarTrigger } from './MenuBarTrigger'
+import { MenuShortcut } from './MenuShortcut'
 
-export function EditMenu(_props: MenuBarProps) {
+export function EditMenu(props: MenuBarProps) {
     return (
         <Menu>
             <MenuBarTrigger label="Edit" />
             <Menu.Portal>
                 <Menu.Content placement="bottom" align="start">
-                    {null}
+                    <Menu.Item onPress={props.onUndo} disabled={!props.canUndo}>
+                        <Menu.ItemTitle>Undo</Menu.ItemTitle>
+                        <MenuShortcut keys="⌘Z" />
+                    </Menu.Item>
+                    <Menu.Item onPress={props.onRedo} disabled={!props.canRedo}>
+                        <Menu.ItemTitle>Redo</Menu.ItemTitle>
+                        <MenuShortcut keys="⌘Y" />
+                    </Menu.Item>
+                    <Separator />
+                    <Menu.Item onPress={props.onCut} disabled={props.disabled}>
+                        <Menu.ItemTitle>Cut</Menu.ItemTitle>
+                        <MenuShortcut keys="⌘X" />
+                    </Menu.Item>
+                    <Menu.Item onPress={props.onCopy} disabled={props.disabled}>
+                        <Menu.ItemTitle>Copy</Menu.ItemTitle>
+                        <MenuShortcut keys="⌘C" />
+                    </Menu.Item>
+                    <Menu.Item onPress={props.onPaste} disabled={props.disabled}>
+                        <Menu.ItemTitle>Paste</Menu.ItemTitle>
+                        <MenuShortcut keys="⌘V" />
+                    </Menu.Item>
+                    <Menu.Sub>
+                        <Menu.SubTrigger>
+                            <Menu.ItemTitle>Paste special</Menu.ItemTitle>
+                        </Menu.SubTrigger>
+                        <Menu.SubContent>
+                            <Menu.Item onPress={props.onPasteValues} disabled={props.disabled}>
+                                <Menu.ItemTitle>Values only</Menu.ItemTitle>
+                                <MenuShortcut keys="⌘⇧V" />
+                            </Menu.Item>
+                            <Menu.Item onPress={props.onPasteFormat} disabled={props.disabled}>
+                                <Menu.ItemTitle>Format only</Menu.ItemTitle>
+                                <MenuShortcut keys="⌘⌥V" />
+                            </Menu.Item>
+                        </Menu.SubContent>
+                    </Menu.Sub>
+                    <Separator />
+                    <Menu.Item onPress={props.onOpenFindReplace}>
+                        <Menu.ItemTitle>Find and replace</Menu.ItemTitle>
+                        <MenuShortcut keys="⌘⇧H" />
+                    </Menu.Item>
                 </Menu.Content>
             </Menu.Portal>
         </Menu>
