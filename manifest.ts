7a97591a01081ffc4a110f56aea245c5b6f79f64
Nathan Stitt <nathan@stitt.org>
2026-09-17 15:48:27 -0500

    chore(calc): require @tinycld/core >=0.5.1

--- manifest.ts
+++ manifest.ts
@@ -28,7 +28,7 @@ const manifest = {
     },
     repository: { url: 'https://github.com/tinycld/calc' },
     dependencies: ['drive'],
-    peerVersions: { '@tinycld/core': '>=0.1.0 <0.2.0' },
+    peerVersions: { '@tinycld/core': '>=0.5.1 <0.6.0' },
 }
 
 export default manifest
