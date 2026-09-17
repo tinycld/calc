68e0f8e2efec18380fa046c3865565c94ec92dbf
Nathan Stitt <nathan@stitt.org>
2026-09-17 15:51:05 -0500

    chore: release v0.3.2

--- manifest.ts
+++ manifest.ts
@@ -1,7 +1,7 @@
 const manifest = {
     name: 'Calc',
     slug: 'calc',
-    version: '0.3.1',
+    version: '0.3.2',
     description: 'Collaborative spreadsheets for your server',
     routes: { directory: 'screens' },
     nav: {
